45d6c80bf3140fb38f822570562d1f09db989324
ivie-gif <emwiongbon_ivie@yahoo.com>
2020-12-14 21:37:34 +0100

    git commit background section and footer section added

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import 'materialize-css'
+
+import { Row, Col, Slide, Slider, Caption, Select, Icon, Button} from 'react-materialize'
+
+
+
+const Index = () => {
+    return (
+        <div>
+        
+        <Slider
+  fullscreen={false}
+  options={{
+    duration: 500,
+    height: 450,
+    indicators: false,
+    interval: 6000
+  }}
+>
+  <Slide image={<img alt="" src="https://cdn.pixabay.com/photo/2013/01/20/09/23/rice-paddies-75590__340.jpg" className="hero-img" />}>
+    <Caption placement="center">
+      <h5 className="button-h5">
+        Welcome to our Farm
+      </h5>
+      <h1 className=" white-text button-h1 ">
+        PRODUCTS
+      </h1>
+      <h6 className="button-h6">
+        GROWN WITH LOVE
+      </h6>
+      <Button
+    large
+    node="a"
+    style={{
+      marginRight: '5px'
+    }}
+    waves="light"
+    className="hero-button"
+  >
+    Shop now
+  </Button>
+    </Caption>
+  </Slide>
+  <Slide image={<img alt="" src="https://media.istockphoto.com/photos/fresh-vegetables-with-circle-in-the-center-for-copy-space-picture-id1184422591?b=1&k=6&m=1184422591&s=170667a&w=0&h=SR6FOtr0-K-XOluYUMGI4pA8Zy-yGH-Zmw1WnshJfWA="/>}>
+  <Caption placement="center">
+      <h6 className="button-h5">
+        High-Quality Organic Products
+      </h6>
+      <h1 className=" white-text button-veggie ">
+        VEGETABLES
+      </h1>
+      <h6 className="button-h6">
+        WITHOUT HARMFUL ADDITIVES
+      </h6>
+      <Button
+    large
+    node="a"
+    style={{
+      marginRight: '5px'
+    }}
+    waves="light"
+    className="hero-button"
+  >
+    Shop now
+  </Button>
+    </Caption>
+  </Slide>
+  <Slide image={<img alt="" src="https://cdn.pixabay.com/photo/2016/10/06/14/40/apples-1719198__340.jpg" className="hero-img" />}>
+  <Caption placement="center">
+      <h5 className="button-h5">
+        Fresh Organic Produce
+      </h5>
+      <h1 className=" white-text button-h1">
+        FRUITS
+      </h1>
+      <h6 className="button-h6">
+        WITH LOTS OF VITAMINS
+      </h6>
+      <Button
+    large
+    node="a"
+    style={{
+      marginRight: '5px'
+    }}
+    waves="light"
+    className="hero-button"
+  >
+    Shop now
+  </Button>
+    </Caption>
+  </Slide>
+</Slider>
+
+        </div>
+    )
+}
+export default Index;
---
+++ src/components/Landing/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Benefits from '../HeroSection/index' 
+import Footer from '../Footer/index'
+
+const Index = () => {
+    return(
+        <div>
+            <Benefits />
+            <Footer />
+        </div>
+    )
+}
+export default Index;
