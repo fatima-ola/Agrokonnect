ab6b4c38202a403b4286363d4b5553838602240d
Fatima <fatimaolasunkanmi01@gmail.com>
2020-12-25 19:13:23 +0100

    eslint

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,6 @@
 import firebase from 'firebase';
 import 'firebase/auth';
 import 'firebase/firebase-storage'
-import firebase from "firebase";
 import 'firebase/auth';
 import 'firebase/firestore'
 
@@ -25,10 +24,4 @@ let firebaseConfig = {
 
   export const facebookProvider = new firebase.auth.FacebookAuthProvider();
 
-  firebase.initializeApp(firebaseConfig);
 
-  export const auth = firebase.auth();
-
-  export const firestore = firebase.firestore();
-
-  export const provider = new firebase.auth.GoogleAuthProvider();
