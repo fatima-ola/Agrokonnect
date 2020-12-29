9a21b61e81d19474a7018973e82f9560ae65cf54
Fatima <fatimaolasunkanmi01@gmail.com>
2020-12-29 17:59:43 +0100

    worked on responsiveness

--- src/components/Clients/index.js
+++ src/components/Clients/index.js
@@ -22,13 +22,13 @@ return(
       >
       <Slide>
               <Caption placement="center" className="dark black-text">
-              <i class="fa fa-quote-left" aria-hidden="true"></i>
+              <i className="fa fa-quote-left" aria-hidden="true"></i>
                 <h5 className="agro">
                   AgroKonnect always satisfy my food needs. I appreciate the fact that they provide me with
                   quality products everytime.
                 </h5>
                 < div className="image">
-                  <img src="MEEE.jpg" alt="" className="circle team-img" />
+                  <img src="MEEE.jpg" alt="Dolapo" className="circle team-img responsive-img" />
                   <div className="names">
                     <h5>DOLAPO</h5>
                     <h6>Client</h6>
@@ -41,12 +41,12 @@ return(
 
             <Slide >
               <Caption placement="center" className="dark black-text">
-              <i class="fa fa-quote-left" aria-hidden="true"></i>
+              <i className="fa fa-quote-left" aria-hidden="true"></i>
                 <h5 className="agro">
                   Best food plug ever. Thanks for making life easy for me.
                 </h5>
                 <div className="image">
-                  <img src="/fatima.png" alt="" className="circle team-img"/>
+                  <img src="/fatima.png" alt="Fatima" className="circle team-img responsive-img" />
                   <div className="names">
                     <h5>FATIMA</h5>
                     <h6>Client</h6>
@@ -58,13 +58,13 @@ return(
             <Slide >
               <Caption placement="center" className="dark black-text">
                 
-              <i class="fa fa-quote-left" aria-hidden="true"></i>
+              <i className="fa fa-quote-left" aria-hidden="true"></i>
                 <h5 className="agro">
                   I do not have to worry about qualitative food produces,
                     all thanks to AgroKonnet for the organic products they offer.
                 </h5>
                 <div className="image">
-                  <img className="circle team-img" alt="" src="/ivie.png"/>
+                  <img className="circle team-img responsive-img" alt="Ive" src="/ivie.png"/>
                   <div className="names">
                     <h5>IVIE</h5>
                     <h6>Client</h6>
